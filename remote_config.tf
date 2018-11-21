25604f37673ce9f9fff1af25a27da6cc6abf0525
defacer <saibalaji@outlook.com>
2018-11-21 23:02:05 +0530

    Update remote_config.tf

--- remote_config.tf
+++ remote_config.tf
@@ -2,5 +2,6 @@ terraform {
   backend "http" {
     update_method = "PUT"
     address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/BzWpUzUkJ51A35VmeauLzYPiotok1CRDyMDtl9u07Is/n/gse00014954/b/smartoffers-test/o/"
+    skip_cert_verification = true
   }
 }
