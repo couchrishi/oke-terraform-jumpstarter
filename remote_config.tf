cb797e8c7daf4dbafae1fa6ed9c2f5615beb4f79
defacer <saibalaji@outlook.com>
2018-12-04 17:32:16 +0530

    Update remote_config.tf

--- remote_config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "http" {
-    update_method = "PUT"
-    address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/IRE1FqRLa6tXQw2xqrNcIGikCd0wu7JcLBKN_7yC77g/n/gse00014373/b/remotetfstate/o/terraform.tfstate"
-    skip_cert_verification = "true"
-  }
-}
