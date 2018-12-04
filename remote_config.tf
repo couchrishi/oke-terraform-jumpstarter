ff7232a0a5fe063b949ecf169cf5e0c09012b1c3
defacer <saibalaji@outlook.com>
2018-12-04 17:07:17 +0530

    OCI changes
    OCI changes

--- remote_config.tf
+++ remote_config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "http" {
     update_method = "PUT"
-    address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/BzWpUzUkJ51A35VmeauLzYPiotok1CRDyMDtl9u07Is/n/gse00014954/b/smartoffers-test/o/"
+    address       = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/IRE1FqRLa6tXQw2xqrNcIGikCd0wu7JcLBKN_7yC77g/n/gse00014373/b/remotetfstate/o/terraform.tfstate"
     skip_cert_verification = "true"
   }
 }
